1fe0f044b7cff522d1f8416e565241958c3c298c
Sarp Misoglu <sarp.misoglu@pomona.edu>
2018-08-08 17:18:44 +0300

    better text cleaning and preprocessing

--- src/main/java/org/TurkishNLP/preprocessing/Cleaner.java
+++ src/main/java/org/TurkishNLP/preprocessing/Cleaner.java
@@ -3,13 +3,15 @@ package org.TurkishNLP.preprocessing;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 @Slf4j
 public class Cleaner {
-    public static String removeUnwantedCharacters(String str){
+    private static String removeUnwantedCharacters(String str){
         // only alphanumeric characters, whitespace or sentence-ending punctuation
         str = str.replaceAll("[^\\p{L}\\s\\p{N}.,?!']", " ");
         // remove single letter/number words that may be floating around
@@ -18,23 +20,34 @@ public class Cleaner {
         return str;
     }
 
-    public static String removeExtraSpaces(String str) {
-        return str
-                .trim()
-                .replaceAll(" +", " ")
+    private static String removeExtraSpacesFromParagraph(String str) {
+        return removeExtraSpaces(str)
                 .replaceAll("\n+", "\n")
                 .replaceAll("(\n )", "\n");
         // TODO: remove extra spaces near punctuation
     }
 
-    public static String cleanString(String str) {
+    // Removes extra spaces from given string. For paragraphs with
+    // possible extra line breaks use removeExtraSpacesFromParagraph
+    private static String removeExtraSpaces(String str) {
+        return str
+                .trim()
+                .replaceAll(" +", " ");
+        // TODO: remove extra spaces near punctuation
+    }
+
+    private static String cleanParagraph(String str) {
+        return removeExtraSpacesFromParagraph(removeUnwantedCharacters(str));
+    }
+
+    private static String cleanLine(String str) {
         return removeExtraSpaces(removeUnwantedCharacters(str));
     }
 
     public static String cleanFileInMemory(Path filePath) {
         try {
             String fileContent = new String(Files.readAllBytes(filePath), "UTF-8");
-            String clean = cleanString(fileContent);
+            String clean = cleanParagraph(fileContent);
             return clean;
         } catch (IOException e) {
             log.error("Bad filepath {}", filePath);
@@ -46,11 +59,38 @@ public class Cleaner {
         return cleanFileInMemory(Paths.get(filePath));
     }
 
-    public static void cleanFileOnDisk(Path filePath) {
-        // TODO
+    public static Path cleanFileOnDisk(Path filePath) {
+        try {
+            Scanner in = new Scanner(filePath);
+            // First write to a temporary file then move it on the original file
+            Path tempPath = Paths.get(System.getProperty("user.dir"), "data", "processed_files",
+                    filePath.getFileName().toString().split("\\.")[0] + ".temp");
+            if (tempPath.toFile().exists()) {
+                Files.delete(tempPath);
+            }
+            PrintWriter out = new PrintWriter(tempPath.toFile());
+            String line;
+            while(in.hasNextLine()) {
+                line = in.nextLine();
+                String cleaned = cleanLine(line);
+                if(cleaned.isEmpty()) continue;
+                out.println(cleaned);
+            }
+            in.close();
+            out.close();
+            return tempPath;
+        } catch (IOException e) {
+            log.error("Something went wrong cleaning file... Check the input path");
+            return null;
+        }
+    }
+
+    public static Path cleanFileOnDisk(String filePath) {
+        return cleanFileOnDisk(Paths.get(filePath));
     }
 
     public static void main(String[] args) {
-        Cleaner.cleanFileInMemory("data\\corpora\\medium_corpus.txt");
+//        Cleaner.cleanFileInMemory("data\\corpora\\medium_corpus.txt");
+        Cleaner.cleanFileOnDisk(Paths.get("data\\corpora\\short_corpus.txt"));
     }
 }
--- src/main/java/org/TurkishNLP/preprocessing/PreProcessor.java
+++ src/main/java/org/TurkishNLP/preprocessing/PreProcessor.java
@@ -2,7 +2,6 @@ package org.TurkishNLP.preprocessing;
 
 import lombok.extern.slf4j.Slf4j;
 import org.TurkishNLP.shared.Timer;
-import org.TurkishNLP.shared.WriteBuffer;
 import zemberek.morphology.TurkishMorphology;
 import zemberek.morphology.analysis.SentenceAnalysis;
 import zemberek.morphology.analysis.SingleAnalysis;
@@ -11,8 +10,9 @@ import zemberek.morphology.lexicon.DictionaryItem;
 import zemberek.tokenization.TurkishSentenceExtractor;
 import zemberek.core.turkish.PrimaryPos;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -48,7 +48,12 @@ public class PreProcessor {
         return lst;
     }
 
-    public long[] analyzeAndWrite(Scanner sc, WriteBuffer buffer) {
+    /*
+     * @return long[] - array of two elements. [0] is sentenceCount
+     *      [1] is tokenCount
+     *
+     */
+    public long[] analyzeAndWrite(Scanner sc, PrintWriter writer, boolean sideBySideMode) {
         long sentenceCount = 0, tokenCount = 0;
         String currentLine;
         while(sc.hasNextLine()){
@@ -61,25 +66,26 @@ public class PreProcessor {
             List<String> sentences = extractor.fromParagraph(currentLine);
 
             for(String s : sentences){
-                buffer.add(s + " #");
+                if(sideBySideMode) writer.print(s + " #");
                 for(DictionaryItem item : analyzeSentence(s)){
                     if(!item.primaryPos.equals(PrimaryPos.Punctuation)) {
-                        buffer.add(" " + item.getId());
+                        writer.print(" " + item.getId());
                         tokenCount++;
                     }
                 }
-                buffer.add(System.lineSeparator());
+                writer.print(System.lineSeparator());
                 sentenceCount++;
             }
         }
-        buffer.finish();
+        writer.close();
         sc.close();
         return new long[] {sentenceCount, tokenCount};
     }
 
-    public void processFile(String filepath) {
+    // TODO: Make this throw IOEXception instead of a billion try catch statements!!
+    public void processFile(String filepath, boolean inMem) {
         Path outPath = Paths.get(System.getProperty("user.dir"), "data", "processed_files",
-                Paths.get(filepath).getFileName().toString().split("\\.")[0] + ".processed3");
+                Paths.get(filepath).getFileName().toString().split("\\.")[0] + ".processed2");
         if (outPath.toFile().exists()) {
             log.info("A processed file for " + filepath + " already exists, aborting...");
             return;
@@ -88,31 +94,62 @@ public class PreProcessor {
         log.info("Starting processing file " + filepath);
 
         Timer.setTimer();
-        log.info("Cleaning the text in memory...");
-
-        String cleaned = Cleaner.cleanFileInMemory(filepath);
-        if (cleaned == null) return;
+        String cleaned = "";
+        Path tempPath = null;
+        if(inMem) {
+            log.info("Cleaning the text in memory...");
+            cleaned = Cleaner.cleanFileInMemory(filepath);
+            if (cleaned == null) return;
+        } else {
+            log.info("Cleaning the text on disk...");
+            tempPath = Cleaner.cleanFileOnDisk(filepath);
+            if (tempPath == null) return;
+        }
 
         log.info("Completed cleaning text...");
         log.info("Beginning analysis...");
-        // Use a buffer to avoid using too much mem. use 10mb
-        WriteBuffer buffer = new WriteBuffer(outPath, 10000);
 
-        // Reading file line by line.
-        Scanner sc = new Scanner(cleaned);
+        PrintWriter pw;
+        try {
+            // Use a buffer to avoid using too much mem. use 10mb
+            pw = new PrintWriter(outPath.toFile());
+        } catch(IOException e) {
+            log.error("This should never happen but we must please the Java god");
+            return;
+        }
 
-        long[] analysisCounts = analyzeAndWrite(sc, buffer);
+        // Scanner to read cleaned corpus line by line
+        Scanner sc = null;
+        if(inMem){
+            sc = new Scanner(cleaned);
+        } else {
+            try {
+                sc = new Scanner(tempPath.toFile());
+            } catch (IOException e) {
+                log.error("This should never happen but we must please the Java god");
+                return;
+            }
+        }
+
+        long[] analysisCounts = analyzeAndWrite(sc, pw, false);
+
+        // If we did this on disk we should clean up after ourselves
+        if(!inMem) try {
+            Files.delete(tempPath);
+        } catch(IOException e) {
+            log.error("This should never happen but we must please the Java god");
+            return;
+        }
 
         Timer.endTimer();
         log.info("Finished processing file " + filepath);
         log.info("Processed {} sentences and {} tokens in " + Timer.results(), analysisCounts[0], analysisCounts[1]);
     }
-
-
+q
     // ****************************** USED FOR TESTING ****************************** \\
     public static void main ( String[] args) throws IOException {
 //        System.out.println(pp.analyzeSentence("Nevşehir'deki mitingin ardından Adıyaman'a geçerek yurttaşlarla bir araya geldi."));
         PreProcessor pp = new PreProcessor();
-        pp.processFile("data\\corpora\\medium_corpus.txt");
+        pp.processFile("data\\corpora\\medium_corpus.txt", false);
     }
 }
